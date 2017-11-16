ac628b8892b4a282d9e481e385fe2484b7a0cf98
Oskar Polak <oskarpolak@vp.pl>
2017-11-16 09:32:31 +0100

    sad

--- src/Task6.java
+++ src/Task6.java
@@ -7,7 +7,7 @@ public class Task6 {
 
         String[] numbers = scanner.nextLine().split(" ");
 
-
+        long startTime = System.nanoTime();
         for(int i = 0; i < numbers.length; i++){
             int localSum = 0;
             for(int x = i; x < numbers.length; x++){
@@ -15,9 +15,25 @@ public class Task6 {
             }
             System.out.println(localSum);
         }
+        long stopTime = System.nanoTime();
+        System.out.println("1 algorytm: " + (stopTime - startTime));
+        ///////////////////////////////////////////
 
 
-
+        long startTime1 = System.nanoTime();
+        int sum = 0;
+        int currentSum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum += Integer.parseInt(numbers[i]);
+        }
+        System.out.println(sum);
+        for(int i = 1; i < numbers.length; i++){
+            currentSum += Integer.parseInt(numbers[i-1]);
+            System.out.println(sum - currentSum);
+        }
+        long stopTime1 = System.nanoTime();
+        System.out.println("2 algorytm: " + (stopTime1 - startTime1));
+        ;
 
     }
 }
